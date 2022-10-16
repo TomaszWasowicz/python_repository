af9f198a05d217140f223ae00615bf0b56e8686f
tomasz.wasowicz@gmail.com <10yearsbackidbefullstack>
2022-10-16 17:08:16 +0200

    04.02.2022

--- exercises/missing values in a n-elemenet list.py
+++ exercises/missing values in a n-elemenet list.py
@@ -1,7 +1,12 @@
-def missing_values(range_of, container):
-    return list(set(range(container)) - set(range_of))
+def missing_values(elements, container):
+    return list(set(range(container)) - set(elements))
 
 
 print(missing_values([1, 2, 4, 6, 7, 9, 20], 40))
 
 
+def brakujące_wartości(elementy, pojemnik):
+    return list(set(range(pojemnik)) - set(elementy))
+
+
+print(brakujące_wartości([2, 8, 5, 9, 1, 16], 20))
